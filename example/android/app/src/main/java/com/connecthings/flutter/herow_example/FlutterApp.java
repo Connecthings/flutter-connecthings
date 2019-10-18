43967ac12a08722824d3ee4e9cd6eb839a377ca1
Ludovic Vimont <ludovic.vimont@connecthings.com>
2019-10-18 14:10:53 +0200

    Update app demo to support Android 10 & Androidx

--- example/android/app/src/main/java/com/connecthings/flutter/herow_example/FlutterApp.java
+++ example/android/app/src/main/java/com/connecthings/flutter/herow_example/FlutterApp.java
@@ -9,16 +9,19 @@ import com.connecthings.util.connection.Url;
 import io.flutter.app.FlutterApplication;
 
 public class FlutterApp extends FlutterApplication {
-
     public void onCreate() {
         super.onCreate();
-        HerowInitializer.getInstance().initContext(this)
+
+        final HerowInitializer herowInitializer = HerowInitializer.getInstance();
+
+        herowInitializer.initContext(this)
                 .initUrlType(Url.UrlType.PRE_PROD)
                 .initApp("SDK_ID", "SDK_KEY")
                 .synchronize();
-        HerowInitializer.getInstance().addPermissionToAsk(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        herowInitializer.addPermissionToAsk(Manifest.permission.ACCESS_FINE_LOCATION);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            HerowInitializer.getInstance().addPermissionToAsk(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+            herowInitializer.addPermissionToAsk(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
         }
     }
 }
--- example/android/app/src/main/java/com/connecthings/flutter/herow_example/MainActivity.java
+++ example/android/app/src/main/java/com/connecthings/flutter/herow_example/MainActivity.java
@@ -2,16 +2,13 @@ package com.connecthings.flutter.herow_example;
 
 import android.os.Bundle;
 
-import com.connecthings.herow.HerowInitializer;
-
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-    HerowInitializer.getInstance().askPermissions();
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
 }
